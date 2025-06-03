d63942974f3ff92735c268480866b29341d476b9
SahilGurav45 <sahilgurav47@gmail.com>
2025-06-03 17:02:52 +0530

    add on github

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './app.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home/Home';
+import Success from './pages/Success/Success';
+import NotFound from './pages/NotFound/NotFound';
+
+const App = () => {
+  return (
+    <>
+      
+     <Router>
+      <Routes>
+
+     <Route path='/' element={<Home/>} />
+     <Route path='/success' element={<Success/>} />
+     <Route path='*' element={<NotFound/>} />
+
+      </Routes>
+     </Router>
+
+     <Toaster/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Zatka RESTO</div>
+          <div className="right">
+            <p>JM ROAD PUNE</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By SAHILGURAV</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By SAHILGURAV.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
